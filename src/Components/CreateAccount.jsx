f91d673f9e5fd59ee1fb48a612ba830d0da07ff0
BenRiekes <95994689+BenRiekes@users.noreply.github.com>
2023-03-08 22:27:27 -0800

    Create Account Modal

--- src/Components/CreateAccount.jsx
+++ src/Components/CreateAccount.jsx
@@ -6,81 +6,107 @@ import { useState, useRef, useEffect } from "react";
 import { 
     Modal, ModalHeader, ModalBody, ModalContent, Input,
     ModalOverlay, ModalFooter, Button, useDisclosure, ModalCloseButton, Radio, 
-    RadioGroup, Stack, FormControl, FormLabel, FormErrorMessage, FormHelperText
+    RadioGroup, HStack, FormControl, FormLabel, FormErrorMessage, FormHelperText, 
+    VStack, StackDivider, Box, Checkbox
 } from "@chakra-ui/react";
 
 
 const CreateAccount = () => {
 
-    const [genderValue, setGenderValue] = React.useState('1')
-    const [ageValue, setAgeValue] = React.useState('1')
-    const [termValue, setTermValue] = React.useState('1')
     const { isOpen, onOpen, onClose } = useDisclosure(); 
 
-    return <div>
-        
-        <button onClick={onOpen}>Create Account</button>
-        <Modal isCentered onClose = {onClose} isOpen = {isOpen} motionPreset = 'slideInBottom'>
+    const [ageValue, setAgeValue] = useState(''); 
+    const [genderValue, setGenderValue] = useState('');
+   
+    return (
 
-        <ModalOverlay />
+        <>
+            <Button onClick={onOpen}>Create Account</Button>
 
-        <ModalContent>
+            <Modal 
+                size = 'md'
+                scrollBehavior = 'inside'
+                motionPreset = 'slideInBottom'
+                isOpen = {isOpen} isCentered onClose = {onClose}
+                
+            >
 
-            <ModalHeader>Sign Up</ModalHeader>
-            <ModalCloseButton />
+                <ModalOverlay />
 
-            <ModalBody>
+                <ModalContent>
 
-                <FormControl>
-                    <FormLabel m={2}>Username</FormLabel>
-                    <Input type = 'username' />
-                    
-                    <FormLabel>Email Address</FormLabel>
-                    <Input type = 'email' />
-                    <FormHelperText>We'll never share your email</FormHelperText>
+                    <ModalHeader>Create Account</ModalHeader>
+                    <ModalCloseButton />
 
-                    <FormLabel>Password</FormLabel>
-                    <Input type = 'password' />
-                    <FormHelperText></FormHelperText>
+                    <ModalBody>
 
-                    <FormLabel>Confirm Password</FormLabel>
-                    <Input type = 'password' />
+                        <FormControl>
 
-                    <FormLabel>What is your Gender?</FormLabel>
-                    <RadioGroup onChange={setGenderValue} value={genderValue} type="gender">
-                        <Stack direction='row'>
-                            <Radio value='1'>Female</Radio>
-                            <Radio value='2'>Male</Radio>
-                            <Radio value='3'>Other</Radio>
-                        </Stack>
-                    </RadioGroup>
+                            <VStack
+                                align = 'stretch'
+                                spacing = {5}
+                                divider = {<StackDivider borderColor='gray.200' />}
+                            >
 
+                                <Box>
+                                    <FormLabel>Email Address</FormLabel>
+                                    <Input type = 'email' />
+                                    <FormHelperText>We'll never share your email</FormHelperText>
+                                </Box>
 
-                    <FormLabel>Are you Over 18 Years Old?</FormLabel>
-                    <RadioGroup onChange={setAgeValue} value={ageValue} type="age">
-                        <Stack direction='row'>
-                            <Radio value='1'>No</Radio>
-                            <Radio value='2'>Yes</Radio>
-                        </Stack>
-                    </RadioGroup>
+                                <Box>
+                                    <FormLabel m={2}>Username</FormLabel>
+                                    <Input type = 'username' />
+                                </Box>
+                                
+                                
+                                <Box>
+                                    <FormLabel>Password</FormLabel>
+                                    <Input type = 'password' />
+                                    <FormHelperText>Password must be 6 characters, alphanumeric </FormHelperText>   
+                                </Box>
 
-                    <FormLabel>Do You Agree To the Terms And Conditions</FormLabel>
-                    <RadioGroup onChange={setTermValue} value={termValue} type="term">
-                        <Stack direction='row'>
-                            <Radio value='1'>No</Radio>
-                            <Radio value='2'>Yes</Radio>
-                        </Stack>
-                    </RadioGroup>
-                    
-                    <center>
-                        <Button>Create Account</Button>
-                    </center>
-                    
-                </FormControl>
-            </ModalBody>
-        </ModalContent>
-        </Modal>
-    </div>
+                                <Box>
+                                    <FormLabel>Confirm Password</FormLabel>
+                                    <Input type = 'password' />
+                                </Box>
+                                
+                                <Box>
+                                    
+                                    <FormLabel>Gender</FormLabel>
+
+                                    <HStack>
+
+                                        <Checkbox
+                                            size = 'md'
+                                            colorScheme = 'teal'
+                                        >Male</Checkbox>
+                                        
+                                        <Checkbox
+                                            size = 'md'
+                                            colorScheme = 'teal'
+                                        >Female</Checkbox>
+
+                                        <Checkbox
+                                            size = 'md'
+                                            colorScheme = 'teal'
+                                        >Other</Checkbox>
+                                    </HStack>
+
+                                </Box>
+                                
+                                <Button colorScheme = 'teal' varient = 'solid'>Log In</Button>
+                            </VStack>
+                            
+                        </FormControl>
+
+                    </ModalBody>
+
+                </ModalContent>
+
+            </Modal>
+        </>
+    )
 }
 
 export default CreateAccount; 
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -80,7 +80,7 @@ export default function NavBar() {
                     </MenuItem>
 
                     <MenuItem>
-                        <LogIn></LogIn>
+                        <LogIn />
                     </MenuItem>
 
                     <MenuItem>
